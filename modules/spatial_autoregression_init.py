9056a60254564521deb378883c675c009ec6eec3
Ronnit Peter <ronnitpeter@gmail.com>
2023-05-13 12:40:32 -0300

    [Ronnit] - Added spatial autoregression

--- modules/spatial_autoregression_init.py
+++ modules/spatial_autoregression_init.py
@@ -10,6 +10,7 @@ If Spatially Autocorrelated
 '''
 
 import geopandas as gpd
+import libpysal as lps
 import pysal as ps
 import dask_geopandas as dgp
 import matplotlib.pyplot as plt
@@ -17,10 +18,12 @@ import matplotlib.pyplot as plt
 def compute_spatial_autoregression(data_file_path, output_file_path, column_1, column_2):
     gdf = gpd.read_file(data_file_path)
     # Convert GeoDataFrame to a Dask-GeoDataFrame
-    dgdf = dgp.from_dataframe(gdf)
+    dgdf = dgp.from_geopandas(gdf, npartitions=4)
+
+    # dgdf = dgp.from_dataframe(gdf)
 
     # Calculate the spatial weights matrix using Queen's Contiguity and standardize rows
-    weights = ps.lib.weights.Queen.from_dataframe(dgdf)
+    weights = lps.weights.Queen.from_dataframe(dgdf)
     weight.transform = 'r'
 
     # Create dependent variable (y) and independent variable (x)
@@ -28,11 +31,24 @@ def compute_spatial_autoregression(data_file_path, output_file_path, column_1, c
     x = dgdf[[column_2]].to_dask_array(lengths=True)
 
     # Fit the spatial lag model or any spreg model of your choice, but proceed further only with this code if there is the columns are spatially autocorrelated
+    model = ps.model.spreg.ML_Lag(y, x, w=weights, name_y='pop_est', name_x=['gdp_md_est'])
+
     # Print the summary of the model
+    print(model.summary)
+
     # Plot the observed vs predicted values
+    plt.figure(figsize=(9, 9))
+    plt.scatter(y, model.predy, color='blue')
+    plt.plot([y.min(), y.max()], [y.min(), y.max()], color='red')
+    plt.title('Observed vs Predicted Values: Spatial Lag Model')
+    plt.xlabel('Observed Values')
+    plt.ylabel('Predicted Values')
+    plt.savefig(f"{output_file_path}_observed_vs_predicted.jpeg")
+    print(f"Plot saved at: {output_file_path}_observed_vs_predicted.jpeg")
 
 if __name__ == '__main__':
-    data_file_path = gpd.dataset.get_path('naturalearth_lowres')
+    data_file_path = gpd.datasets.get_path('naturalearth_lowres')
     output_file_path = "/home/y4xxh/Documents/SSTD_GeoSpatial/images/spatial_autoregression/spatial_lag_model"
     column_1 = 'pop_est'
     column_2 = 'gdp_md_est'
+    compute_spatial_autoregression(data_file_path, output_file_path, column_1, column_2)
