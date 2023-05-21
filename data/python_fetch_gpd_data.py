6a2b6852c35d60fa22934cb565666d20f150c1da
Ronnit Peter <ronnitpeter@gmail.com>
2023-05-20 21:19:07 -0300

    [Ronnit] - Changed plots

--- data/python_fetch_gpd_data.py
+++ data/python_fetch_gpd_data.py
@@ -1,4 +1,4 @@
 import geopandas as gpd
 
-data = gpd.datasets.get_path('naturalearth_lowres')
+data = gpd.datasets.get_path('naturalearth_cities')
 print(data)
--- modules/changed_bvsa_plots.py
+++ modules/changed_bvsa_plots.py
@@ -10,6 +10,14 @@ def compute_bivariate_spatial_autocorr(data_file_path, cols, output_file_path):
     
     df = gpd.read_file(data_file_path)
     
+    # Filter the dataframe based on pop_est and gdp_md_est
+    # df = df[(df['continent'] == 'Europe') | (df['continent'] == 'Asia')]
+    df = df[(df['pop_est'] > 100) | (df['gdp_md_est'] < 16)]
+
+    # Add check for number of rows
+    if len(df) < 2:
+        raise ValueError("Not enough rows in the dataframe after filtering. Please adjust the filtering conditions.")
+    
     for col in cols:
         assert col in df.columns, f"Column {col} does not exist in the dataframe."
     
@@ -40,10 +48,10 @@ def compute_bivariate_spatial_autocorr(data_file_path, cols, output_file_path):
         # Create a new weights matrix for the filtered data
         weights_sig = lps.weights.Queen.from_dataframe(df_sig)
         weights_sig.transform = 'r'
-
+                
         # Recalculate local_moran for the filtered data
         local_moran_sig = esda.moran.Moran_Local(np.nan_to_num(df_sig[f"{col}_std"]), weights_sig)
-
+                
         fig, ax = lisa_cluster(local_moran_sig, df_sig, p=0.05) # Plotting LISA cluster map with significant locations only
 
         plt.title(f"Local Moran's I LISA Cluster Map for {col}")
@@ -53,7 +61,6 @@ def compute_bivariate_spatial_autocorr(data_file_path, cols, output_file_path):
     if len(cols) == 2:
         bivariate_moran = esda.moran.Moran_BV(df[f"{cols[0]}_std"], df[f"{cols[1]}_std"], weights)
         print(f"Bivariate Moran's I between {cols[0]} and {cols[1]}: {bivariate_moran.I:.4f}, p-value: {bivariate_moran.p_sim:.4f}")
-
 
 if __name__ == '__main__':
     data_file_path = gpd.datasets.get_path('naturalearth_lowres')
